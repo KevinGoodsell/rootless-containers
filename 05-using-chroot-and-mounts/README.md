b870981ed7e3279c1dd11f9e4e6c9679be6c9873
Kevin Goodsell <kevin-opensource@omegacrash.net>
2024-03-29 19:45:42 -0700

    Add part 5, chroot and mounts

---
+++ 05-using-chroot-and-mounts/README.md
@@ -0,0 +1,54 @@
+# Part 5: Using chroot and Mounts
+
+In this part we'll use the root file system we set up in Part 4 as the file
+system for our container. We'll add mounts, and use `chroot(2)` to make it the
+root for processes running inside the container.
+
+Most of what we mount in the container will be bind mounts, which means we're
+just sharing part of a file system from the host inside the container. `/proc`
+will be a new mount, not a bind mount (which is necessary in order to have a
+view of our own PID namespace rather than the parent namespace, as we saw back
+in Part 3).
+
+The example program also bind mounts `/etc/resolv.conf` from the host system to
+enable name resolution by just mirroring what the host is using. A static copy
+of the file could also work depending on the situation, but this approach causes
+the container file to be updated when the host file is updated, since they are
+just different views of the same file.
+
+The new example program requires an additional argument, the path to the root
+file system:
+
+    $ python3 example05.py --hostname container ../alpine/alpine-root/ -- ash -l
+
+Note that now the program to run is `ash` instead of `bash`. The shell in the
+base Alpine system is `ash`, and `bash` isn't available unless you install it
+separately. 
+
+Now that we're inside the container with the new root file system, we can do
+things like install additional software:
+
+    # apk update
+    fetch http://mirrors.edge.kernel.org/alpine/latest-stable/main/x86_64/APKINDEX.tar.gz
+    v3.19.1-298-g03c4df03632 [http://mirrors.edge.kernel.org/alpine/latest-stable/main]
+    OK: 5441 distinct packages available
+    # apk add bash
+    (1/4) Installing ncurses-terminfo-base (6.4_p20231125-r0)
+    (2/4) Installing libncursesw (6.4_p20231125-r0)
+    (3/4) Installing readline (8.2.1-r2)
+    (4/4) Installing bash (5.2.21-r0)
+    Executing bash-5.2.21-r0.post-install
+    Executing busybox-1.36.1-r15.trigger
+    OK: 12 MiB in 29 packages
+
+We can also add a non-root user to use in future parts:
+
+    # adduser -s /bin/bash alpine
+
+You might notice various things that are still leaking in from the host
+environment. For example, `env` may show a lot of environment variables that
+aren't relevant inside the container. Depending on your groups in the host
+system, `id` might show a lot of `nobody` groups:
+
+    # id
+    uid=0(root) gid=0(root) groups=65534(nobody),65534(nobody),65534(nobody),65534(nobody),65534(nobody),65534(nobody),65534(nobody),65534(nobody),65534(nobody),65534(nobody),65534(nobody),65534(nobody),65534(nobody),0(root)
---
+++ 05-using-chroot-and-mounts/example05.py
@@ -0,0 +1,134 @@
+import argparse
+import grp
+import mmap
+import os
+from pathlib import Path
+import pwd
+import signal
+from socket import sethostname
+import subprocess
+import sys
+
+from lib import libc
+
+
+def read_subuids(uid: int) -> tuple[int, int]:
+    name = pwd.getpwuid(uid)[0]
+    return read_ids(uid, name, '/etc/subuid')
+
+
+def read_subgids(gid: int) -> tuple[int, int]:
+    name = grp.getgrgid(gid)[0]
+    return read_ids(gid, name, '/etc/subgid')
+
+
+def read_ids(id_: int, name: str, filename: str) -> tuple[int, int]:
+    # subuid/subgid can use names or numbers, so we check for both.
+    names = [str(id_), name]
+    with open(filename, 'r') as f:
+        for line in f:
+            entry, start, count = line.split(':')
+            if entry in names:
+                return (int(start), int(count))
+
+    raise Exception(f'User {name} not found in {filename}')
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser(
+            description='Run a command in a new namespace')
+    parser.add_argument(
+            '--hostname',
+            help='set hostname in the new namespace')
+    parser.add_argument(
+            'root',
+            help='root file system')
+    parser.add_argument(
+            'cmd',
+            nargs='+',
+            help='command (and arguments) to run')
+
+    args = parser.parse_args(sys.argv[1:])
+
+    uid = os.geteuid()
+    gid = os.getegid()
+
+    subuids = read_subuids(uid)
+    subgids = read_subgids(gid)
+
+    uid_maps = [
+            '0', str(uid), '1',
+            '1', str(subuids[0]), str(subuids[1]),
+    ]
+
+    gid_maps = [
+            '0', str(gid), '1',
+            '1', str(subgids[0]), str(subgids[1]),
+    ]
+
+    # Make a shared memory semaphore
+    sem = mmap.mmap(
+            -1,
+            libc.SIZEOF_SEM_T,
+            mmap.MAP_SHARED | mmap.MAP_ANONYMOUS)
+
+    libc.sem_init(sem, True, 0)
+
+    def child() -> int:
+        # Wait for parent to set up uidmap and gidmap.
+        libc.sem_wait(sem)
+
+        if args.hostname is not None:
+            sethostname(args.hostname)
+
+        mounts = [
+                ['--bind', '/dev/null', 'dev/null'],
+                ['--bind', '/dev/full', 'dev/full'],
+                ['--bind', '/dev/ptmx', 'dev/ptmx'],
+                ['--bind', '/dev/random', 'dev/random'],
+                ['--bind', '/dev/urandom', 'dev/urandom'],
+                ['--bind', '/dev/zero', 'dev/zero'],
+                ['--bind', '/dev/tty', 'dev/tty'],
+                ['-t', 'proc', 'proc', 'proc'],
+                # Sysfs can't be mounted in a user namespace unless it's also
+                # in a network namespace. Apparently this has something to do
+                # with accessing network devices via /sys/class/net.
+                # For some reason bind mount /sys requires --rbind.
+                ['--rbind', '/sys', 'sys'],
+                ['--bind', '/etc/resolv.conf', 'etc/resolv.conf'],
+        ]
+
+        for *mount_args, target in mounts:
+            full_target = str(Path(args.root) / target)
+            subprocess.run(['mount'] + mount_args + [full_target], check=True)
+
+        os.chroot(args.root)
+        # chroot doesn't actually change the current directory:
+        os.chdir(args.root)
+
+        os.execvp(args.cmd[0], args.cmd)
+
+    pid = libc.clone(
+            child,
+            100_000,
+            signal.SIGCHLD | libc.CLONE_NEWUSER | libc.CLONE_NEWPID |
+            libc.CLONE_NEWUTS | libc.CLONE_NEWNS)
+
+    subprocess.run(['newuidmap', str(pid)] + uid_maps, check=True)
+    subprocess.run(['newgidmap', str(pid)] + gid_maps, check=True)
+
+    # Signal child that its environment is ready
+    libc.sem_post(sem)
+
+    (_, status) = os.waitpid(pid, 0)
+    exitcode = os.waitstatus_to_exitcode(status)
+
+    if exitcode < 0:
+        print(f'child process exited with signal {-exitcode}', file=sys.stderr)
+        return 1
+
+    return exitcode
+
+
+if __name__ == '__main__':
+    sys.exit(main())
