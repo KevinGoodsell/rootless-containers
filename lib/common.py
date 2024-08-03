89d610d0dc3330431e449cea4dc94d35c05cecc5
Kevin Goodsell <kevin-opensource@omegacrash.net>
2024-08-03 14:05:38 -0700

    Add a few docstrings

--- lib/common.py
+++ lib/common.py
@@ -4,6 +4,10 @@ import ctypes
 
 
 def load_lib(name: str) -> ctypes.CDLL:
+    '''
+    Return a CDLL for the named library, or raise an Exception if it's not
+    found.
+    '''
     fullname = find_library(name)
     if fullname is None:
         raise Exception(f'Library not found: {name}')
@@ -12,5 +16,8 @@ def load_lib(name: str) -> ctypes.CDLL:
 
 
 def get_os_error() -> OSError:
+    '''
+    Fetch errno and return an OSError based on it.
+    '''
     e = ctypes.get_errno()
     return OSError(e, os.strerror(e))
